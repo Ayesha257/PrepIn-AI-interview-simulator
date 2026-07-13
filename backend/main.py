b4f0892582c192435170715ebc9cf3c718fd4229
Ayesha257 <ayeshaamer257@gmail.com>
2026-07-14 00:14:43 +0500

    connect deployment

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://prepin.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
